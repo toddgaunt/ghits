86f61ffaf49e0bca68ee6940e17d5f6a9addf25f
Todd Gaunt <toddgaunt@protonmail.ch>
2018-10-25 10:28:51 -0400

    Add basic argument parsing to scraper, and initial JSON pull

--- bin/scraper.py
+++ bin/scraper.py
@@ -1,7 +1,26 @@
 #!/usr/bin/env python3
 
-import
+import requests
+import json
+import argparse
+import sys
 
-github_api = "https://api.github.com/"
+github_api = "https://api.github.com"
 
 def main():
+    parser = argparse.ArgumentParser(description="Pull some repo issues")
+    parser.add_argument('account', type=str,
+                        help="Github user account to pull from")
+    parser.add_argument('repo', type=str,
+                        help="Github repository associated with an account")
+    args = parser.parse_args(sys.argv[1:])
+    r = requests.get(f'{github_api}/repos/{args.account}/{args.repo}')
+    print(f'{github_api}/repos/{args.account}/{args.repo}')
+    if not r.ok:
+        print("Get request did not return OK")
+        sys.exit(-1)
+    repo_data = json.loads(r.text)
+    print(repo_data)
+
+if __name__ == "__main__":
+    main()
