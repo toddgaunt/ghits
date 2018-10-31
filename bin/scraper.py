0ba9a5662fba24c645c05c4d24911189947052b9
Todd Gaunt <toddgaunt@protonmail.ch>
2018-10-31 19:23:44 -0400

    Improve scraper output, and add option to choose how many files to scrape

--- bin/scraper.py
+++ bin/scraper.py
@@ -8,9 +8,9 @@ from unidiff import PatchSet
 
 github_api = "https://api.github.com"
 
-def get_closed_pulls(account, repo):
-    r = requests.get(f'{github_api}/repos/{account}/{repo}/pulls?state=closed&page=0')
-    print(f'{github_api}/repos/{account}/{repo}/pulls?state=closed&page=0')
+def get_closed_pulls(account, repo, n):
+    r = requests.get(f'{github_api}/repos/{account}/{repo}/pulls?state=closed&page=0&per_page={n}')
+    print(f'{github_api}/repos/{account}/{repo}/pulls?state=closed&page=0&per_page={n}')
     if not r.ok:
         print(f'Get request did not return OK ({r.status_code})')
         sys.exit(-1)
@@ -24,8 +24,10 @@ def main():
                         help="Github user account to pull from")
     parser.add_argument('repo', type=str,
                         help="Github repository associated with an account")
+    parser.add_argument('-n', type=int, default=30,
+                        help="Number of issues to pull (default=30)")
     args = parser.parse_args(sys.argv[1:])
-    pulls = get_closed_pulls(args.account, args.repo)
+    pulls = get_closed_pulls(args.account, args.repo, args.n)
     #print(json.dumps(pulls, indent=4, sort_keys=True))
     out = []
     for pull in pulls:
@@ -34,11 +36,11 @@ def main():
         diff = r.text
         patch = PatchSet(diff)
         tmp = {"query": pull["title"] + " " + pull["body"]}
-        tmp["added"] = patch.added_files
-        tmp["modified"] = patch.modified_files
-        tmp["removed"] = patch.removed_files
+        tmp["added"] = list(map(lambda x: x.source_file, patch.added_files))
+        tmp["modified"] = list(map(lambda x: x.source_file, patch.modified_files))
+        tmp["removed"] = list(map(lambda x: x.source_file, patch.removed_files))
         out.append(tmp)
-    print(out)
+    print(json.dumps(out, indent=4, sort_keys=True))
 
 if __name__ == "__main__":
     main()
