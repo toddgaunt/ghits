66f9595103e5516ff5788de267a8db8d3734fe6f
Francisco Santana <f.santana31@outlook.com>
2018-11-26 20:29:51 -0500

    add 3rd TF-IDF variant
    we now have 3 LM and 3 TF-IDF variants to make our feature vectors

--- src/main/java/Team1/TFIDF.java
+++ src/main/java/Team1/TFIDF.java
@@ -4,67 +4,96 @@ import org.apache.lucene.search.similarities.TFIDFSimilarity;
 import org.apache.lucene.util.BytesRef;
 
 public class TFIDF {
-	public static TFIDFSimilarity BnnBnnSim() {
-		return new TFIDFSimilarity() {
-			@Override
-			public float tf(float freq) {
-				if (freq > 0)
-					return 1.0f;
-				return 0.0f;
-			}
+    public static TFIDFSimilarity BnnBnnSim() {
+        return new TFIDFSimilarity() {
+            @Override
+            public float tf(float freq) {
+                if (freq > 0)
+                    return 1.0f;
+                return 0.0f;
+            }
 
-			@Override
-			public float idf(long docFreq, long docCount) {
-				return docFreq;
-			}
+            @Override
+            public float idf(long docFreq, long docCount) {
+                return docFreq;
+            }
 
-			@Override
-			public float lengthNorm(int length) {
-				return length; // TODO not sure how to implement
-			}
+            @Override
+            public float lengthNorm(int length) {
+                return length; // TODO not sure how to implement
+            }
 
-			@Override
-			public float sloppyFreq(int distance) {
-				return 0;
-			}
+            @Override
+            public float sloppyFreq(int distance) {
+                return 0;
+            }
 
-			@Override
-			public float scorePayload(int doc, int start, int end, BytesRef payload) {
-				return 0;
-			}
+            @Override
+            public float scorePayload(int doc, int start, int end, BytesRef payload) {
+                return 0;
+            }
 
-		};
-	}
+        };
+    }
 
-	public static TFIDFSimilarity LncLtnSim() {
-		return new TFIDFSimilarity() {
-			@Override
-			public float tf(float freq) {
-				return 1.0f + (float)Math.log10(freq);
-			}
+    public static TFIDFSimilarity LncLtnSim() {
+        return new TFIDFSimilarity() {
+            @Override
+            public float tf(float freq) {
+                return 1.0f + (float) Math.log10(freq);
+            }
 
-			@Override
-			public float idf(long docFreq, long docCount) {
-				if (docFreq == 0)
-					return 1;
-				return (float)Math.log10(docCount/docFreq);
-			}
+            @Override
+            public float idf(long docFreq, long docCount) {
+                if (docFreq == 0)
+                    return 1;
+                return (float) Math.log10(docCount / docFreq);
+            }
 
-			@Override
-			public float lengthNorm(int length) {
-				return length;
-			}
+            @Override
+            public float lengthNorm(int length) {
+                return length;
+            }
 
-			@Override
-			public float sloppyFreq(int distance) {
-				return 0;
-			}
+            @Override
+            public float sloppyFreq(int distance) {
+                return 0;
+            }
 
-			@Override
-			public float scorePayload(int doc, int start, int end, BytesRef payload) {
-				return 0;
-			}
+            @Override
+            public float scorePayload(int doc, int start, int end, BytesRef payload) {
+                return 0;
+            }
 
-		};
-	}
+        };
+    }
+
+    public static TFIDFSimilarity AnnApnSim() {
+        return new TFIDFSimilarity() {
+            @Override
+            public float tf(float freq) {
+                return 0.5f + (0.5f * freq / freq);
+            }
+
+            @Override
+            public float idf(long docFreq, long docCount) {
+                return docFreq;
+            }
+
+            @Override
+            public float lengthNorm(int length) {
+                return length;
+            }
+
+            @Override
+            public float sloppyFreq(int distance) {
+                return 0;
+            }
+
+            @Override
+            public float scorePayload(int doc, int start, int end, BytesRef payload) {
+                return 0;
+            }
+        };
+    }
 }
