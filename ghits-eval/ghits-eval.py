ad8db1c3f605e1b501a2e4bf2373529967a794a0
Francisco Santana <f.santana31@outlook.com>
2018-11-30 22:58:18 -0500

    setup eval to make index

--- ghits-eval/ghits-eval.py
+++ ghits-eval/ghits-eval.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 import os
 import sys
@@ -10,17 +10,53 @@ import re
 
 # argument parsing
 a = argparse.ArgumentParser(
-    description='evaluates ghits retrieval, given a file with the relevant repos for each query and another file with the list of repos retrieved by our application.')
-a.add_argument("--q", help='in addition to summary evaluation, give evaluation for each query', default=False, action="store_true",
-               dest="q")
-a.add_argument("--qrel", help='path of file with list of relevant repos for each query', dest="rel_file") # required=True)
-a.add_argument("--m", help='shows a specific measure, shows [xxx] by default', default="tf-idf", dest="method")
-a.add_argument("--results", help='path the file with the list of repos retrieved by our application', dest="results_file", required=True)
+    description='evaluates the ghits tool, given a file with the truth data and a specific measure')
+# a.add_argument("-d", help="enable debug prinouts", default=False)
+a.add_argument("-p", help='path to tool makefile', default="../", dest="tool_path")
+a.add_argument("-r", help="path to repo source", default="TestRepo", dest="repo_path")
+a.add_argument("-t", help='path of file with list of relevant repos for each query', dest="rel_file", required=True)
+a.add_argument("-m", help='<MAP | P@R | F1>', default="MAP", dest="method")
 arguments = a.parse_args()
 
-# get commands
-# command = [arguments.q, arguments.rel_file, arguments.results_file]
-# read
-results_file = open(arguments.results_file)
-data = json.load(results_file)
-print(data)
+tool_dir = arguments.tool_path
+test_file = arguments.rel_file
+repo_dir = arguments.repo_path
+rel_data = json.load(open(test_file))
+
+# build the index
+cwd = os.getcwd()  # get current directory
+try:
+    process = subprocess.Popen("make index ARGS={repo}".format(repo=repo_dir), shell=True, stdout=subprocess.PIPE,
+                               cwd=tool_dir, universal_newlines=True)
+    for stdout_line in iter(process.stdout.readline, ""):
+        print(stdout_line)
+    process.stdout.close()
+    return_code = process.wait()
+    if return_code:
+        raise subprocess.CalledProcessError(return_code, "make index")
+finally:
+    os.chdir(cwd)
+
+# #
+# try:
+#     process = subprocess.Popen("make run", shell=True, stdout=subprocess.PIPE, stdin=subprocess.PIPE,
+#                                cwd=tool_dir, universal_newlines=True)
+#     for stdout_line in iter(process.stdout.readline, ""):
+#         print(stdout_line)
+#     process.stdout.close()
+#
+#     # run queries
+#     for query, files in rel_data.items():
+#         print(query)
+#         process.stdin.write(query)
+#         # for f in files:
+#         #     #print(f)
+#
+#     return_code = process.wait()
+#     if return_code:
+#         raise subprocess.CalledProcessError(return_code, "make run")
+# finally:
+#     os.chdir(cwd)
+
+
+
